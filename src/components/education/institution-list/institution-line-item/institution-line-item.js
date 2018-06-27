4da8556c771f7b5c10858d3c651885c40ba5df25
Drew MacLaren <drmaclar@cisco.com>
2018-06-27 18:54:57 -0400

    updated basic education layout and styling

--- src/components/education/institution-list/institution-line-item/institution-line-item.js
+++ src/components/education/institution-list/institution-line-item/institution-line-item.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-//import moment from 'moment';
+import moment from 'moment';
 import './institution-line-item.css';
 
 class InstitutionLineItem extends Component {
   render() {
+
+    const degreeItems = this.props.institutionData.degrees.map((degree) =>
+      <li className="degree-list-item">{degree.type} in {degree.field} - {moment(degree.completionDate).format('YYYY')}</li>
+    );
+
+    const minorItems = this.props.institutionData.minors.map((minor) =>
+      <li className="minor-list-item">Minor in {minor.field} - {moment(minor.completionDate).format('YYYY')}</li>
+    );
+
     return (
         <div className="institution-line-item">
-            <p>{this.props.institutionData.name}</p>
+            <div className="institution-name">{this.props.institutionData.name}</div>
+            <ul className="degrees-list">
+              {degreeItems}
+              {minorItems}
+            </ul>
         </div>
     );
   }

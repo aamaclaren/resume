1f199a7efba24d6dbfec85eba76a578ad9549a2a
Drew MacLaren <drmaclar@cisco.com>
2018-06-28 15:17:32 -0400

    Updated education layout - added degrees and minors list

--- src/components/education/institution-list/institution-line-item/institution-line-item.js
+++ src/components/education/institution-list/institution-line-item/institution-line-item.js
@@ -6,11 +6,15 @@ class InstitutionLineItem extends Component {
   render() {
 
     const degreeItems = this.props.institutionData.degrees.map((degree) =>
-      <li className="degree-list-item">{degree.type} in {degree.field} - {moment(degree.completionDate).format('YYYY')}</li>
+      <li className="degree-list-item">
+        {degree.type} in {degree.field} - {moment(degree.completionDate).format('YYYY')}
+      </li>
     );
 
     const minorItems = this.props.institutionData.minors.map((minor) =>
-      <li className="minor-list-item">Minor in {minor.field} - {moment(minor.completionDate).format('YYYY')}</li>
+      <li className="minor-list-item">
+        Minor in {minor.field} - {moment(minor.completionDate).format('YYYY')}
+      </li>
     );
 
     return (
